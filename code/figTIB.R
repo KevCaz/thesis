970227531b0855c0dc7af6ef4cab2d27584ac583
Kevin Cazelles <kevin.cazelles@um2.fr>
2016-07-22 09:32:20 -0400

    imporved figure1

--- code/figTIB.R
+++ code/figTIB.R
@@ -47,32 +47,32 @@ cex_let <- 1.4
 pdf(filename, height=wi2, width=wi2)
 
   layout(matrix(c(3,3,4,4,1,1,1,2), nrow=2, byrow=TRUE), heights=c(.8,1))
-  par(mar=c(3,3,1,.5), mgp=c(1.6,0,0))
+  par(mar=c(4,4,1,.5), mgp=c(1.8,0.6,0))
   ##
   plot0(c(0,100),c(0,0.44))
-  mycol <- c(colg3,colb1)
+  mycol <- c(colb1,colg3)
   for (i in 2:3){
     lines(A, B[,i], col=mycol[i-1], lwd=3)
     lines(A, C[,i], col=mycol[i-1], lwd=3, lty=4)
   }
-  title(xlab="Richesse spécifique sur l'île", ylab="Probabilité d'un évènement", cex.lab=1.4)
+  title(xlab="Richesse spécifique sur l'île", ylab="Probabilité d'un évènement", cex.lab=2)
   box(bty="l", lwd=2.4, lend=2)
   ##
   points(c(16.53,82.92), c(0.08,0.08), col=c(colb1,colg3), cex=2.2, pch=19)
-  legend("top", c("Colonisation", "Extinction"), cex=1.2, lty=c(1,4), ncol=1, bty="n", col=colg3, lwd=3, seg.len=3)
-  axis(1,at=c(16.53,82.92), lwd=0, lwd.ticks=1, labels=c("1", "2"), tck=.04, cex.lab=1.2)
+  legend("top", c("Colonisation", "Extinction"), cex=1.5, lty=c(1,4), ncol=1, bty="n", col=colg3, lwd=3, seg.len=4)
+  # axis(1,at=c(16.53,82.92), lwd=0, lwd.ticks=1, labels=c("1", "2"), tck=.04, cex.axis=1.8)
   addLetter(3, cex=cex_let)
   ##
   par(mar=c(4,0,1,.5))
   plot0(c(0,10),c(0,10))
   abline(h=8, lwd=2, col=colg3)
-  text(0, 9, "Continent", font=2, pos=4, col=colg3, cex=2)
+  text(0, 9, "CONTINENT", font=2, pos=4, col=colg3, cex=2)
   rect(.5,6.2,6,3.5, col=colg3, border=NA)
   arrows2(x0=mean(c(.5,6)), y0=8, y1=6.2, col=colg2, border=NA, prophead=FALSE, cex.hh=1.8)
   rect(7.5,3,9.5,1.8, col=colb1, border=NA)
   arrows2(x0=mean(c(7.5,9.5)), y0=8, y1=3, col=colg2, border=NA, prophead=FALSE)
 
-  par(mar=c(3,3,3,.5))
+  par(mar=c(1,1,3,.5))
   plotImage(file="./code/fig4MW.png")
   addLetter(1, cex=cex_let)
   ##
